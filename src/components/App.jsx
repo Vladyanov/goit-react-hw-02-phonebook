d61eb1cf59872daf5a84dcdbde068f7c12577958
Vlad Pazyna <vladpazyna@gmail.com>
2023-01-26 00:35:24 +0200

    some progress

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,127 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+import css from './app.module.scss';
+
+class App extends Component {
+  state = {
+    contacts: [
+      {
+        id: nanoid(),
+        name: 'Vlad',
+        number: '06233133576',
+      },
+      {
+        id: nanoid(),
+        name: 'Anton',
+        number: '09514061',
+      },
+    ],
+    name: '',
+    number: '',
+    filter: '',
+  };
+
+  removeContact = id => {
+    this.setState(({ contacts }) => {
+      const newContacts = contacts.filter(item => item.id !== id);
+      return { contacts: newContacts };
+    });
+  };
+
+  isDuplicate(name, phoneNumber) {
+    const normalizedName = name.toLowerCase();
+
+    const { contacts } = this.state;
+    const isUnique = contacts.find(({ name, number }) => {
+      return (
+        name.toLocaleLowerCase() === normalizedName && phoneNumber === number
+      );
+    });
+    return isUnique;
+  }
+
+  addContact = e => {
+    e.preventDefault();
+    this.setState(prevState => {
+      const { contacts, name, number } = prevState;
+      if (this.isDuplicate(name, number)) {
+        return alert(`${name}: ${number} is already in contacts list`);
+      }
+      const newContact = {
+        id: nanoid(),
+        name,
+        number,
+      };
+      return { contacts: [newContact, ...contacts], name: '', number: '' };
+    });
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    console.log(name, value);
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    const { addContact, handleChange, removeContact } = this;
+    const { name, number } = this.state;
+
+    const { contacts } = this.state;
+    const items = contacts.map(({ id, name, number }) => (
+      <li key={id} className={css.item}>
+        {name}: {number}{' '}
+        <button onClick={() => removeContact(id)} className={css.btn}>
+          Delete
+        </button>
+      </li>
+    ));
+
+    return (
+      <>
+        <div className={css.block}>
+          <h3 className={css.title}>Phone Book</h3>
+          <form action="" onSubmit={addContact}>
+            <div className={css.formGroup}>
+              <label className={css.label}>Name</label>
+              <input
+                type="text"
+                name="name"
+                value={name}
+                onChange={handleChange}
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                required
+              />
+            </div>
+
+            <div className={css.formGroup}>
+              <label className={css.label}>Number</label>
+              <input
+                type="tel"
+                name="number"
+                value={number}
+                onChange={handleChange}
+                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                required
+              />
+            </div>
+
+            <button className={css.btn} type="submit">
+              Add contact
+            </button>
+          </form>
+        </div>
+        <div className={css.block}>
+          <h3 className={css.title}>Contacts</h3>
+          <label className={css.label}>Find contacts by name</label>
+          <input type="text" name="filter" />
+          <ol className={css.list}>{items}</ol>
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
